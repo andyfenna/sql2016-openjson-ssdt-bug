fd1a8d9f53dc586eb8f735bca85f50c5547e19e3
Andy Fenna <andrew.fenna@laterooms.com>
2016-06-28 10:08:41 +0100

    Initial commit

---
+++ OpenJsonBug/GetResultsCrossApply.sql
@@ -0,0 +1,55 @@
+﻿CREATE PROCEDURE dbo.GetResultsCrossApply
+AS
+BEGIN 
+	SELECT JSON_VALUE(x.[value], '$.Id') AS ID
+	FROM dbo.Result e
+	CROSS APPLY OPENJSON(e.JsonResults, '$.Response.Results') AS x
+END
+
+/*
+
+	This does not compile, it throws:-
+
+	SQL46010: Incorrect syntax near e.	OpenJsonBug	C:\dbgit\OpenJsonBug\OpenJsonBug\GetResultsCrossApply.sql	6	
+
+	It seems to be a problem with the cross apply passing a column instead of varibale, SSDT will throw an error.
+
+	If this code is deployed within SSMS the DDL does infact complile and gives me the correct results when executing 	
+	this SQL:-
+
+	CREATE TABLE dbo.Result
+	(
+		ID INT IDENTITY(1,1) PRIMARY KEY,
+		JsonResults NVARCHAR(MAX) NOT NULL
+	)
+	GO
+
+	CREATE PROCEDURE dbo.GetResultsCrossApply
+		 @json nvarchar(max)
+	AS
+	BEGIN 
+		SELECT JSON_VALUE(x.[value], '$.Id') AS ID
+		FROM OPENJSON(@json, '$.Response.Results') AS x
+	END
+
+	GO
+
+	DECLARE @json NVARCHAR(MAX) = '{
+		"Response": {
+			"Results": [{
+				"Id": 1
+			}, {
+				"Id": 2
+			}, {
+				"Id": 3
+			}]
+		}
+	}';
+
+	INSERT INTO dbo.Result (JsonResults)
+	VALUES (@json)
+	GO
+
+	EXEC dbo.GetResultsCrossApply
+
+*/
---
+++ OpenJsonBug/GetResultsFrom.sql
@@ -0,0 +1,44 @@
+﻿CREATE PROCEDURE dbo.GetResultsFrom
+	 @json nvarchar(max)
+AS
+BEGIN 
+	SELECT JSON_VALUE(x.[value], '$.Id') AS ID
+	FROM OPENJSON(@json, '$.Response.Results') AS x
+END
+
+
+/*
+
+	This seems to compile OK SSDT, however it does throw a warning:-
+
+	SQL71005: The reference to the column x.[value] could not be resolved
+
+	I'm unsure why the above column is unresolved as the below script compiles 
+	file in SSMS:
+
+	CREATE PROCEDURE dbo.GetResultsFrom
+		@json NVARCHAR(MAX)
+	AS
+	BEGIN 
+		SELECT JSON_VALUE(x.[value], '$.Id') AS ID
+		FROM OPENJSON(@json, '$.Response.Results') AS x
+	END
+
+	GO
+
+	DECLARE @JSON NVARCHAR(MAX) = '{
+		"Response": {
+			"Results": [{
+				"Id": 1
+			}, {
+				"Id": 2
+			}, {
+				"Id": 3
+			}]
+		}
+	}';
+
+	EXEC dbo.GetResultsFrom
+		 @json
+
+*/
